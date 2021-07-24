5e650b19bbfc1d23ab212d0c7af13e07573b9979
leulseged3 <leulseged3@gmail.com>
2021-07-25 00:30:21 +0300

    add and setup react-native-navigation(wix).

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 /**
  * @format
  */
+import { Navigation } from 'react-native-navigation'
 
-import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('WelcomeScreen', () => App)
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'WelcomeScreen'
+            }
+          }
+        ]
+      }
+    }
+  })
+})
